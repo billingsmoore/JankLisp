827a1c0aacbd7b68af51eb134a16ed835e260ed4
Jacob Moore <billingsmoore@gmail.com>
2023-04-28 16:33:48 -0400

    ch 7 done

--- parsing.c
+++ parsing.c
@@ -6,6 +6,38 @@
 #include <editline/readline.h>
 #include <editline/history.h>
 
+long eval_op(long x, char* op, long y) {
+	if (strcmp(op, "+") == 0) { return x + y;};
+	if (strcmp(op, "-") == 0) { return x - y;};
+	if (strcmp(op, "*") == 0) { return x * y;};
+	if (strcmp(op, "/") == 0) { return x / y;};
+	return 0;
+}
+
+
+long eval(mpc_ast_t* t) {
+
+	/* If tagged as number return it directy*/
+	if (strstr(t->tag, "number")) {
+		return atoi(t->contents);
+	}
+
+	/* The operator is always the second child*/
+	char* op = t->children[1]->contents;
+
+	/* We store the third child in 'x' */
+	long x = eval(t->children[2]);
+
+	/* Iterate the remaining children and combining. */
+	int i = 3;
+	while (strstr(t->children[i]->tag, "expr")) {
+		x = eval_op(x, op, eval(t->children[i]));
+		i++;
+	}
+
+	return x;
+}
+
 int main(int argc, char** argv) {
 
 	/* Create Some Parsers*/
@@ -24,7 +56,7 @@ int main(int argc, char** argv) {
 		Number, Operator, Expr, Lispy);
 
 	/* Print Version and Exit Information */
-	puts("JankLisp Version 0.0.0.0.1");
+	puts("JankLisp Version 0.0.0.0.3");
 	puts("Press Ctrl+c to Exit\n");
 	
 	/* In a never ending loop */
@@ -40,7 +72,8 @@ int main(int argc, char** argv) {
 		mpc_result_t r;
 		if (mpc_parse("<stdin>", input, Lispy, &r)) {
 			/* On Success Print the AST*/
-			mpc_ast_print(r.output);
+			long result = eval(r.output);
+			printf("%li\n", result);
 			mpc_ast_delete(r.output);
 		} else {
 			mpc_err_print(r.error);
